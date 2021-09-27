d6edcf14b3d6a961d464a0235626f3c2162f33a6
smollalex <smollalex86@gmail.com>
2021-09-27 16:51:00 +0600

    optimize css, home mobile

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  plugins: [
+    require('postcss-import'),
+    require('tailwindcss'),
+    require('tailwindcss/nesting')(require('postcss-nesting')),
+    require('autoprefixer'),
+  ]
+}
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: {
+    enabled: true,
+    content: ['./public/**/*.html'],
+  },
+
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
